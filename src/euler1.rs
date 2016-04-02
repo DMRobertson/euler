67310df97732150942074647f4ce85a61750c9a9
David Robertson <david.m.robertson1@gmail.com>
2016-04-03 00:15:45 +0100

    Including the problems as comments

--- src/euler1.rs
+++ src/euler1.rs
@@ -1,18 +1,26 @@
+/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+/// The sum of these multiples is 23.
+
+#[test]
+fn test() {
+	let expected = 23;
+	let result = sum_multiples_3_5(1, 10);
+    assert_eq!(result, expected);
+}
+
+
 fn sum_multiples_3_5 (start: u32, end: u32) -> u32 {
     let mut sum = 0;
     for i in start..end {
         if i % 3 == 0 || i % 5 == 0 {
-            sum += i
+            sum += i;
         }
     }
     sum
 }
 
+/// Find the sum of all the multiples of 3 or 5 below 1000.
 pub fn main() {
-    println!("{}", sum_multiples_3_5(1, 1000))
+    println!("{}", sum_multiples_3_5(1, 1000));
 }
 
-#[test]
-fn test() {
-    assert_eq!(23, sum_multiples_3_5(1, 10))
-}
--- src/euler3.rs
+++ src/euler3.rs
@@ -1,3 +1,12 @@
+/// The prime factors of 13195 are 5, 7, 13 and 29.
+
+#[test]
+fn test(){
+	let expected = vec![5, 7, 13, 29];
+	let result = prime_factors(13195);
+	assert_eq!(result, expected);
+}
+
 // fn is_prime(n: u64) -> bool {
 	// let bound = (n as f32).sqrt() as u64;
 	// for i in (2..bound+1){
@@ -25,15 +34,10 @@ fn prime_factors(mut n: u64) -> Vec<u64> {
 	factors
 }
 
+/// The prime factors of 13195 are 5, 7, 13 and 29.
+
 fn main() {
 	let factors = prime_factors(600851475143);
 	let max_factor = factors.last().unwrap();
 	println!("{}", max_factor);
-}
-
-#[test]
-fn test(){
-	let expected = vec![5, 7, 13, 29];
-	let result = prime_factors(13195);
-	assert_eq!(result, expected);
 }
