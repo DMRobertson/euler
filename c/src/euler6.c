0bb8d3c04d6e3d409e24c865aac93e8c4c70feee
David Robertson <david.m.robertson1@gmail.com>
2016-06-14 14:09:02 +0100

    C: Problem 6

---
+++ c/src/euler6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <assert.h>
+
+/*
+The sum of the squares of the first ten natural numbers is
+	1² + 2² + ... + 10² = 385
+The square of the sum of the first ten natural numbers is,
+	(1 + 2 + ... + 10)² = 55² = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+int square_sum_one_upto(int limit){
+	int sum = 0;
+	for (int i = 1; i <= limit; i++){
+		sum += i;
+	}
+	return sum * sum;
+}
+
+int sum_squares_upto(int limit){
+	int sum = 0;
+	for (int i = 1; i <= limit; i++){
+		sum += i * i;
+	}
+	return sum;
+}
+
+int main(){
+	assert(sum_squares_upto(10) == 385);
+	assert(square_sum_one_upto(10) == 3025);
+	int answer = square_sum_one_upto(100) - sum_squares_upto(100);
+	printf("%i\n", answer);
+	return 0;
+}
