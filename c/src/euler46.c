b819c152bf3b1929fc0db2a3ca66976edd67aa94
David Robertson <david.m.robertson1@gmail.com>
2016-12-20 12:19:50 +0000

    C: Problem 46

---
+++ c/src/euler46.c
@@ -0,0 +1,55 @@
+//prepare: -lm
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <math.h>
+#include "number_theory.c"
+#include "vector.c"
+
+/* It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
+
+9 = 7 + 2×12
+15 = 7 + 2×22
+21 = 3 + 2×32
+25 = 7 + 2×32
+27 = 19 + 2×22
+33 = 31 + 2×12
+
+It turns out that the conjecture was false.
+
+What is the smallest odd composite that cannot be written as the sum of a prime and twice a square? */
+
+bool satisfies_conjecture(const unsigned n){
+	if (is_prime(n)){
+		return true;
+	}
+	for (unsigned root = 1; root < (unsigned) sqrt(n); root++){
+		if (is_prime(n - 2 * root * root)){
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(){
+	assert(satisfies_conjecture(9));
+	assert(satisfies_conjecture(15));
+	assert(satisfies_conjecture(21));
+	assert(satisfies_conjecture(25));
+	assert(satisfies_conjecture(27));
+	assert(satisfies_conjecture(33));
+	unsigned candidate = 9;
+	vector* primes = vnew(4);
+	vpush(primes, 2);
+	vpush(primes, 3);
+	vpush(primes, 5);
+	vpush(primes, 7);
+	while (true){
+		if (!satisfies_conjecture(candidate)){
+			break;
+		}
+		candidate += 2;
+	}
+	printf("%u\n", candidate);
+	return 0;
+}
